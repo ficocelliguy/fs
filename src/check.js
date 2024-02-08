3f0e2e19843e09af72ad5d69794803c363b54b69
Michael Ficocelli <ficocemt@gmail.com>
2024-02-08 10:27:39 -0500

    Improved logging and package type identification

--- src/check.js
+++ src/check.js
@@ -1,14 +1,18 @@
-const {cliCollisionPackages, nodeDefaultPackages} = require("./util/const");
+const {cliCollisionPackages, nodeDefaultPackages, color} = require("./util/const");
 const {run} = require("./util/run");
 const DAY_IN_MS = 1000 * 60 * 60 * 24
-const FRESH_THRESHOLD = DAY_IN_MS * 365 * 3;
+const FRESH_THRESHOLD = DAY_IN_MS * 365 * 2;
+const STALE_THRESHOLD = DAY_IN_MS * 365 * 4;
 
-const getDependencies = () => {
-    const result = run('npm ls -json')
+const getDependencies = (all, recursive) => {
+    const result = run(`npm ls --json ${recursive ? "--all" : ""}`)
+    const commonPackages = cliCollisionPackages.concat(nodeDefaultPackages);
 
-    const dependencies = Object.keys(result?.dependencies || {}).filter(d => !d.match(/(@npm|^npm-)/i));
-    console.log(dependencies);
-    return dependencies;
+    return Object.keys(result?.dependencies || {}).filter(d => {
+        const npmInternal = d.match(/(@npm|^npm-)/i);
+        const shouldBeChecked = all ? true : commonPackages.includes(d)
+        return !npmInternal && shouldBeChecked
+    });
 }
 
 const getDependencyUpdateTimes = (dependencies) => {
@@ -16,7 +20,7 @@ const getDependencyUpdateTimes = (dependencies) => {
     for (let i = 0; i < dependencies.length; i++) {
         const framework = dependencies[i];
         printProgress(i +1, dependencies.length);
-        const data = run(`npm view ${framework} -json`);
+        const data = run(`npm view ${framework} --json`);
         const latest = data?.["dist-tags"]?.latest;
         const modified = data?.time?.[latest]
         modifiedDateDependencies.push({
@@ -36,10 +40,10 @@ const formatTimeSince = (msTimeSinceUpdate) => {
     const months = Math.floor( (days % 365) / 30);
     const years = Math.floor(days / 356);
 
-    return `${format(years, " year")}${format(months, " month", false)}`;
+    return `${format(years, " year")}${format(months, " month")}`;
 }
 
-const format = (num, label, comma=true) => num ? `${num}${label}${num > 1 ? "s" : ""}${comma ? ",":""} ` : ""
+const format = (num, label) => num ? `${num}${label}${num > 1 ? "s" : ""} ` : ""
 
 const auditFs = () =>
     run(
@@ -47,16 +51,38 @@ const auditFs = () =>
         false, true
     );
 
-const checkForOutdatedDependencies = () => {
-    const dependencies = getDependencies();
-    const updates = getDependencyUpdateTimes(dependencies);
+const checkForOutdatedDependencies = (all, recursive) => {
+    const dependencies = getDependencies(all, recursive);
+    console.log(`Found ${dependencies.length} target dependencies to age-check.`)
 
-    for (const framework of updates) {
+    const updates = getDependencyUpdateTimes(dependencies);
+    const defaultPackages = updates.filter(d => nodeDefaultPackages.includes(d.name))
+    const cliPackages = updates.filter(d => cliCollisionPackages.includes(d.name))
+    const other = updates.filter(d => !nodeDefaultPackages.includes(d.name) && !cliCollisionPackages.includes(d.name))
+
+    if (defaultPackages.length) {
+        console.log("These npm packages already come by default with NodeJS, and they have not been updated in some time in npm:")
+        printFrameworkAges(defaultPackages);
+    }
+    if (cliPackages.length) {
+        console.log("These npm packages may have been installed by mistake, as their name is the same as npm cli command, and they have not been updated in some time:")
+        printFrameworkAges(cliPackages);
+    }
+    if (other.length) {
+        console.log("These npm packages have not been updated in some time:")
+        printFrameworkAges(other);
+    }
+
+}
+
+const printFrameworkAges = (frameworkUpdates) => {
+    for (const framework of frameworkUpdates) {
         const timeSinceUpdate = (Date.now() - new Date(framework.modified));
-        if (timeSinceUpdate > FRESH_THRESHOLD) {
-            console.log(`${framework.name}: ${formatTimeSince(timeSinceUpdate)}`);
-        } else {
-            console.log(`${framework.name}: Still alive!`)
+        const name = `${color.FgCyan}${framework.name}${color.Reset}:`
+        if (timeSinceUpdate > STALE_THRESHOLD) {
+            console.log(`${name} ${color.Bright}${color.FgRed}${formatTimeSince(timeSinceUpdate)}${color.Reset}`);
+        } else if (timeSinceUpdate > FRESH_THRESHOLD) {
+            console.log(`${name} ${color.Bright}${color.FgYellow}${formatTimeSince(timeSinceUpdate)}${color.Reset}`);
         }
     }
 }
@@ -66,9 +92,9 @@ function printProgress(current, max){
     process.stdout.write(`Checking for abandoned frameworks, ${current} / ${max} ...           `);
 }
 
-const main = () => {
-    checkForOutdatedDependencies();
-    //auditFs();
+const main = (all = false, recursive = false) => {
+    checkForOutdatedDependencies(all, recursive);
+    auditFs();
 }
 
-main();
+module.exports = main;
--- src/util/const.js
+++ src/util/const.js
@@ -136,7 +136,35 @@ const nodeDefaultPackages = [
     'worker_threads', 
     'zlib']
 
+const color = {
+    Reset: "\x1b[0m",
+    Bright: "\x1b[1m",
+    Dim: "\x1b[2m",
+    Underscore: "\x1b[4m",
+
+    FgBlack: "\x1b[30m",
+    FgRed: "\x1b[31m",
+    FgGreen: "\x1b[32m",
+    FgYellow: "\x1b[33m",
+    FgBlue: "\x1b[34m",
+    FgMagenta: "\x1b[35m",
+    FgCyan: "\x1b[36m",
+    FgWhite: "\x1b[37m",
+    FgGray: "\x1b[90m",
+
+    BgBlack: "\x1b[40m",
+    BgRed: "\x1b[41m",
+    BgGreen: "\x1b[42m",
+    BgYellow: "\x1b[43m",
+    BgBlue: "\x1b[44m",
+    BgMagenta: "\x1b[45m",
+    BgCyan: "\x1b[46m",
+    BgWhite: "\x1b[47m",
+    BgGray: "\x1b[100m"
+}
+
 module.exports = {
     cliCollisionPackages,
     nodeDefaultPackages,
+    color,
 }
